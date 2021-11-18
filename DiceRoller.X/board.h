e380584365f8fd786b1c7ff146bc70cbb7d34894
Issamaysin <renato.pepe.96@gmail.com>
2021-11-18 10:47:41 -0300

    pin e port definitions em um arquivo separado

---
+++ DiceRoller.X/board.h
@@ -0,0 +1,35 @@
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef BOARD_H
+#define	BOARD_H
+      
+#include <stddef.h>
+
+//Portas        
+#define PORT_A 0
+#define PORT_B 1
+#define PORT_C 2
+
+        
+        
+/* Define dos pinos utilizados */
+#define pinDisplay1 4
+#define pinDisplay2 5   
+#define pinDisplay3 6
+#define pinDisplay4 7
+#define portDisplay1 PORT_B
+#define portDisplay2 PORT_A
+#define portDisplay3 PORT_B
+#define portDisplay4 PORT_B
+        
+        
+        
+        
+#endif	/* XC_HEADER_TEMPLATE_H */
+
